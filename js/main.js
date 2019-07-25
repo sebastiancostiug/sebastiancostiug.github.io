02246d690ef73f9fbbfbde450f180a75be4ee29d
Sebastian <sebastian.costiug@gmail.com>
2019-07-25 21:13:49 +0300

    1st version personal pagev.0.3

--- js/main.js
+++ js/main.js
@@ -6,9 +6,9 @@ $(document).ready(function(e) {
 	var grinHead = $("#grinHead");
 	grinHeadPosition = grinHead.position();
 	portfolioBtn.click(function(e) {
-		var presentGrinHeadPosition = grinHead.position();
 		e.preventDefault();
-		var contentSection = $("div.divisor>div.accent");
+		var presentGrinHeadPosition = grinHead.position();
+		var contentSection = $("div.accent");
 		contentSection.animate({ height: "+55vh" }, 1000);
 		menu.fadeOut(500);
 		portfolioBtn.delay(500).fadeOut(500);
@@ -22,14 +22,14 @@ $(document).ready(function(e) {
 			grinHead.fadeOut(500);
 			grinHead.animate(
 				{
-					left   : "3vw",
-					bottom : "34vh"
+					left : "3vw",
+					top  : "37vh"
 				},
 				{
 					step     : function(now, fx) {
-						$(this).css("-webkit-transform", "rotate(" + now * 2 + "deg)");
-						$(this).css("-moz-transform", "rotate(" + now * 2 + "deg)");
-						$(this).css("transform", "rotate(" + now * 2 + "deg)");
+						$(this).css("-webkit-transform", "rotate(" + now * 1.8 + "deg)");
+						$(this).css("-moz-transform", "rotate(" + now * 1.8 + "deg)");
+						$(this).css("transform", "rotate(" + now * 1.8 + "deg)");
 					},
 					duration : 1000
 				},
@@ -57,14 +57,14 @@ $(document).ready(function(e) {
 			grinHead.fadeOut(500);
 			grinHead.animate(
 				{
-					left   : "3vw",
-					bottom : "34vh"
+					left : "3vw",
+					top  : "37vh"
 				},
 				{
 					step     : function(now, fx) {
-						$(this).css("-webkit-transform", "rotate(" + now * 2 + "deg)");
-						$(this).css("-moz-transform", "rotate(" + now * 2 + "deg)");
-						$(this).css("transform", "rotate(" + now * 2 + "deg)");
+						$(this).css("-webkit-transform", "rotate(" + now * 1.8 + "deg)");
+						$(this).css("-moz-transform", "rotate(" + now * 1.8 + "deg)");
+						$(this).css("transform", "rotate(" + now * 1.8 + "deg)");
 					},
 					duration : 1000
 				},
