0ddd980fabc72743661503cb5170d8003f3dc5c6
Sebastian <sebastian.costiug@gmail.com>
2019-07-28 11:02:28 +0300

    mobile alpha 2

--- js/main.js
+++ js/main.js
@@ -9,17 +9,18 @@ $(document).ready(function(e) {
 	var footerHeight = Math.floor($("footer.navMenu").height());
 	var neutralHeight = Math.floor($("section.content>div.neutral").height());
 	var containerHeight = Math.floor($("div.container").height());
+	var containerWidth = Math.floor($("div.container").width());
 	var navMenuToggle = $("#toggle");
 	$("#toggle").change(function() {
 		let percentHight = $("div.accent").height() * 100 / $("div.container").height();
-		if (20 < percentHight && percentHight < 70 && !$(this).is(":checked")) {
+		if (percentHight < 25) {
 			console.log(percentHight + " --IF");
-			$("div.accent").animate({ height: "70%" }, 500);
-		} else if ($(this).is(":checked") && 70 > percentHight && percentHight > 20) {
-			console.log(percentHight + " --ELSE IF");
-			$("div.accent").animate({ height: "40%" }, 500);
-		} else {
-			console.log(percentHight + " --ELSE");
+		} else if ($(this).is(":checked")) {
+			console.log(percentHight + " --ELSE IF MENU CLOSED");
+			$("div.accent").animate({ height: "30%" }, 500);
+		} else if (!$(this).is(":checked")) {
+			console.log(percentHight + " --ELSE IF MENU OPEN");
+			$("div.accent").animate({ height: "50%" }, 500);
 		}
 	});
 	portfolioBtn.click(function(e) {
@@ -40,18 +41,18 @@ $(document).ready(function(e) {
 			next();
 		});
 		menu.delay(500).fadeIn(1000);
-		if (grinHeadPosition.top == presentGrinHeadPosition.top) {
+		if (grinHeadPosition.top == presentGrinHeadPosition.top && containerWidth > 736) {
 			grinHead.fadeOut(500);
 			grinHead.animate(
 				{
-					left : "-14%",
-					top  : "40%"
+					left : "-23%",
+					top  : "23%"
 				},
 				{
 					step     : function(now, fx) {
-						$(this).css("-webkit-transform", "rotate(" + now * 1.8 + "deg)");
-						$(this).css("-moz-transform", "rotate(" + now * 1.8 + "deg)");
-						$(this).css("transform", "rotate(" + now * 1.8 + "deg)");
+						$(this).css("-webkit-transform", "rotate(" + now * 2.5 + "deg)");
+						$(this).css("-moz-transform", "rotate(" + now * 2.5 + "deg)");
+						$(this).css("transform", "rotate(" + now * 2.5 + "deg)");
 					},
 					duration : 1000
 				},
@@ -81,18 +82,18 @@ $(document).ready(function(e) {
 			next();
 		});
 		menu.delay(500).fadeIn(1000);
-		if (grinHeadPosition.top == presentGrinHeadPosition.top) {
+		if (grinHeadPosition.top == presentGrinHeadPosition.top && containerWidth > 736) {
 			grinHead.fadeOut(500);
 			grinHead.animate(
 				{
-					left : "3vw",
-					top  : "37vh"
+					left : "-23%",
+					top  : "23%"
 				},
 				{
 					step     : function(now, fx) {
-						$(this).css("-webkit-transform", "rotate(" + now * 1.8 + "deg)");
-						$(this).css("-moz-transform", "rotate(" + now * 1.8 + "deg)");
-						$(this).css("transform", "rotate(" + now * 1.8 + "deg)");
+						$(this).css("-webkit-transform", "rotate(" + now * 2.5 + "deg)");
+						$(this).css("-moz-transform", "rotate(" + now * 2.5 + "deg)");
+						$(this).css("transform", "rotate(" + now * 2.5 + "deg)");
 					},
 					duration : 1000
 				},
@@ -103,6 +104,8 @@ $(document).ready(function(e) {
 		$("section.portfolio").fadeOut(500);
 		$("section.about").fadeOut(500);
 		$("section.contact").delay(1000).fadeIn(1000);
+		$("#contactForm :input").prop("disabled", true);
+		$("#disabledForm").delay(5000).fadeIn(3000);
 	});
 	aboutBtn.click(function(e) {
 		e.preventDefault();
@@ -122,18 +125,18 @@ $(document).ready(function(e) {
 			next();
 		});
 		menu.delay(500).fadeIn(1000);
-		if (grinHeadPosition.top == presentGrinHeadPosition.top) {
+		if (grinHeadPosition.top == presentGrinHeadPosition.top && containerWidth > 736) {
 			grinHead.fadeOut(500);
 			grinHead.animate(
 				{
-					left : "3vw",
-					top  : "37vh"
+					left : "-23%",
+					top  : "23%"
 				},
 				{
 					step     : function(now, fx) {
-						$(this).css("-webkit-transform", "rotate(" + now * 1.8 + "deg)");
-						$(this).css("-moz-transform", "rotate(" + now * 1.8 + "deg)");
-						$(this).css("transform", "rotate(" + now * 1.8 + "deg)");
+						$(this).css("-webkit-transform", "rotate(" + now * 2.5 + "deg)");
+						$(this).css("-moz-transform", "rotate(" + now * 2.5 + "deg)");
+						$(this).css("transform", "rotate(" + now * 2.5 + "deg)");
 					},
 					duration : 1000
 				},
