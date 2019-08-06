b774c7b64f4360398e0daef37e3ebe3e9c9c9ccd
Sebastian <sebastian.costiug@gmail.com>
2019-08-06 14:58:35 +0300

    fitness_tracker_working_countdowns

--- fitness_tracker/js/countdown.jsx
+++ fitness_tracker/js/countdown.jsx
@@ -3,60 +3,48 @@ class Countdown extends Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			timerOn       : this.props.timerOn, //boolean
-			timerWorkTime : this.props.timerWorkTime, //in seconds
-			timerRestTime : this.props.timerRestTime, //in seconds
-			timerRounds   : this.props.timerRounds //an integer number
+			timerOn             : this.props.timerOn, //boolean
+			timerWorkTime       : this.props.timerWorkTime, //in seconds
+			timerRestTime       : this.props.timerRestTime, //in seconds
+			timerRounds         : this.props.timerRounds, //an integer number
+			roundNumber         : 1,
+			totalWorkoutSeconds : (this.props.timerWorkTime + this.props.timerRestTime) * this.props.timerRounds
 		};
 	}
 	componentDidMount() {
-		if (this.state.timerOn) {
-			if (this.state.timerRounds > 0) {
-				this.workTimer = setInterval(() => {
-					if (this.state.timerWorkTime > 0) {
-						console.log("first timer working");
-						this.setState({
-							timerWorkTime : this.state.timerWorkTime - 1
-						});
-					} else {
-						console.log("first timer done");
-						this.restTimer = setInterval(() => {
-							if (this.state.timerRestTime > 0) {
-								console.log("second timer working");
-								this.setState({
-									timerRestTime : this.state.timerRestTime - 1
-								});
-							} else {
-								console.log("second timer done");
-								this.setState({
-									timerWorkTime : this.props.timerWorkTime,
-									timerRestTime : this.props.timerRestTime,
-									timerRounds   : this.state.timerRounds - 1
-								});
-							}
-						}, 1000);
-					}
-				}, 1000);
-			} else {
-				console.log("both timers done");
-				this.setState({ timerOn: false });
-			}
+		if (this.state.totalWorkoutSeconds > 0) {
+			this.workTimer = setInterval(() => {
+				console.log("first timer working");
+				this.setState({
+					totalWorkoutSeconds : this.state.totalWorkoutSeconds - 1
+				});
+				if (this.state.timerWorkTime > 0) {
+					this.setState({
+						timerWorkTime : this.state.timerWorkTime - 1
+					});
+				} else if (this.state.timerRestTime > 0) {
+					this.setState({
+						timerRestTime : this.state.timerRestTime - 1
+					});
+				} else if (this.state.timerRounds > 0) {
+					this.setState({
+						timerWorkTime : this.props.timerWorkTime - 1,
+						timerRestTime : this.props.timerRestTime,
+						timerRounds   : this.state.timerRounds - 1,
+						roundNumber   : this.state.roundNumber + 1
+					});
+				}
+			}, 1000);
 		}
 	}
 	componentDidUpdate() {
-		if (this.state.timerRestTime <= 0) {
-			console.log("cleared both intervals");
+		if (this.state.totalWorkoutSeconds == 0) {
 			clearInterval(this.workTimer);
-			clearInterval(this.restTimer);
+			console.log(" component did update - cleared both intervals - timer done");
 		}
 	}
-	componentWillUnmount() {
-		clearInterval(this.workTimer);
-		clearInterval(this.restTimer);
-	}
 	render() {
-		let { timerWorkTime, timerRestTime, timerRounds } = this.state;
-		let totalSeconds = (timerWorkTime + timerRestTime) * timerRounds;
+		let totalSeconds = this.state.totalWorkoutSeconds;
 		let totalMinutes = totalSeconds / 60;
 		let roundedTotalMinutes = Math.floor(totalMinutes);
 		let andTotalSeconds = (totalMinutes - roundedTotalMinutes) * 60;
@@ -64,12 +52,14 @@ class Countdown extends Component {
 		return (
 			<Fragment>
 				<div className="Countdown-time">
-					<div>Work time:{timerWorkTime}</div>
-					<div>Rest time:{timerRestTime}</div>
-					<div>Rounds left:{timerRounds}</div>
+					<div>Round:{this.state.roundNumber}</div>
+					<div>Work time:{this.state.timerWorkTime}</div>
+					<div>Rest time:{this.state.timerRestTime}</div>
+					<div>Rounds left:{this.state.timerRounds - 1}</div>
 					<div>
 						Total time left:
-						{roundedTotalMinutes} : {andTotalSeconds}
+						{roundedTotalMinutes} :
+						{roundedAndTotalSeconds}
 					</div>
 				</div>
 			</Fragment>
